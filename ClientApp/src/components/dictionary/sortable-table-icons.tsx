608498bf98ae003bbb0d6b25a8ade6d0ef476e68
Per Ivar Nerseth <perivar@nerseth.com>
2019-07-08 16:56:02 +0200

    converted the icons to functonal components

--- ClientApp/src/components/dictionary/sortable-table-icons.tsx
+++ ClientApp/src/components/dictionary/sortable-table-icons.tsx
@@ -1,4 +1,4 @@
-import React, { Component, CSSProperties } from 'react';
+import React, { CSSProperties, FunctionComponent } from 'react';
 
 interface SortIconProps {
   style?: CSSProperties;
@@ -8,27 +8,19 @@ interface FaIconProps extends SortIconProps {
   icon: string;
 }
 
-class FaIcon extends Component<FaIconProps, any> {
-  render() {
-    const className = `fas ${this.props.icon}`;
-    return <i className={className} style={this.props.style} />;
-  }
-}
+const FaIcon: FunctionComponent<FaIconProps> = props => {
+  const className = `fas ${props.icon}`;
+  return <i className={className} style={props.style} />;
+};
 
-export class SortIconBoth extends Component<SortIconProps, any> {
-  render() {
-    return <FaIcon icon="fa-sort" style={this.props.style} />;
-  }
-}
+export const SortIconBoth: FunctionComponent<SortIconProps> = props => {
+  return <FaIcon icon="fa-sort" style={props.style} />;
+};
 
-export class SortIconAsc extends Component<SortIconProps, any> {
-  render() {
-    return <FaIcon icon="fa-sort-down" style={this.props.style} />;
-  }
-}
+export const SortIconAsc: FunctionComponent<SortIconProps> = props => {
+  return <FaIcon icon="fa-sort-down" style={props.style} />;
+};
 
-export class SortIconDesc extends Component<SortIconProps, any> {
-  render() {
-    return <FaIcon icon="fa-sort-up" style={this.props.style} />;
-  }
-}
+export const SortIconDesc: FunctionComponent<SortIconProps> = props => {
+  return <FaIcon icon="fa-sort-up" style={props.style} />;
+};
