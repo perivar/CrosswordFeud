3a8ea65af65beead92db5e4178773310184c1125
Per Ivar Nerseth <perivar@nerseth.com>
2019-11-05 23:13:11 +0100

    fixed hamburger menu

--- ClientApp/src/components/shared/NavMenu.tsx
+++ ClientApp/src/components/shared/NavMenu.tsx
@@ -8,19 +8,43 @@ interface ILocalState {
 export class NavMenu extends Component<{}, ILocalState> {
   // ref variables
   private menu: React.RefObject<HTMLDivElement>;
+  private hamburger: React.RefObject<HTMLButtonElement>;
 
   constructor(props: any) {
     super(props);
 
     this.menu = React.createRef();
+    this.hamburger = React.createRef();
   }
 
   handleNavBurgerToggle = () => {
+    if (this.hamburger.current) {
+      if (this.hamburger.current.classList.contains('is-active')) {
+        this.hamburger.current.classList.remove('is-active');
+      } else {
+        this.hamburger.current.classList.add('is-active');
+      }
+    }
+
     if (this.menu.current) {
       if (this.menu.current.classList.contains('is-active')) {
         this.menu.current.classList.remove('is-active');
       } else {
         this.menu.current.classList.add('is-active');
+      }
+    }
+  };
+
+  handleNavBurgerClose = () => {
+    if (this.hamburger.current) {
+      if (this.hamburger.current.classList.contains('is-active')) {
+        this.hamburger.current.classList.remove('is-active');
+      }
+    }
+
+    if (this.menu.current) {
+      if (this.menu.current.classList.contains('is-active')) {
+        this.menu.current.classList.remove('is-active');
       }
     }
   };
@@ -36,11 +60,12 @@ export class NavMenu extends Component<{}, ILocalState> {
               </NavLink>
               <button
                 type="button"
-                className="navbar-burger burger button is-white"
+                className="navbar-burger button is-white"
                 aria-label="menu"
                 aria-expanded="false"
                 data-target="navbarBasic"
-                onClick={this.handleNavBurgerToggle}>
+                onClick={this.handleNavBurgerToggle}
+                ref={this.hamburger}>
                 <span aria-hidden="true" />
                 <span aria-hidden="true" />
                 <span aria-hidden="true" />
@@ -49,26 +74,50 @@ export class NavMenu extends Component<{}, ILocalState> {
 
             <div id="navbarBasic" className="navbar-menu" ref={this.menu}>
               <div className="navbar-start">
-                <NavLink className="navbar-item is-tab" to="/dictionary" activeClassName="is-active">
+                <NavLink
+                  className="navbar-item is-tab"
+                  to="/dictionary"
+                  activeClassName="is-active"
+                  onClick={this.handleNavBurgerClose}>
                   Dictionary
                 </NavLink>
-                <NavLink className="navbar-item is-tab" to="/counter" activeClassName="is-active">
+                <NavLink
+                  className="navbar-item is-tab"
+                  to="/counter"
+                  activeClassName="is-active"
+                  onClick={this.handleNavBurgerClose}>
                   Counter
                 </NavLink>
-                <NavLink className="navbar-item is-tab" to="/crossword" activeClassName="is-active">
+                <NavLink
+                  className="navbar-item is-tab"
+                  to="/crossword"
+                  activeClassName="is-active"
+                  onClick={this.handleNavBurgerClose}>
                   Crossword
                 </NavLink>
-                <NavLink className="navbar-item is-tab" to="/forecast" activeClassName="is-active">
+                <NavLink
+                  className="navbar-item is-tab"
+                  to="/forecast"
+                  activeClassName="is-active"
+                  onClick={this.handleNavBurgerClose}>
                   Forecast
                 </NavLink>
               </div>
               <div className="navbar-end">
                 <div className="navbar-item">
                   <div className="buttons">
-                    <NavLink className="button is-info" to="/register" activeClassName="is-active">
+                    <NavLink
+                      className="button is-info"
+                      to="/register"
+                      activeClassName="is-active"
+                      onClick={this.handleNavBurgerClose}>
                       <strong>Sign up</strong>
                     </NavLink>
-                    <NavLink className="button is-light" to="/login" activeClassName="is-active">
+                    <NavLink
+                      className="button is-light"
+                      to="/login"
+                      activeClassName="is-active"
+                      onClick={this.handleNavBurgerClose}>
                       Login
                     </NavLink>
                   </div>
